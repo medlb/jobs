73793aedd2970065d07bae31ce6e31268cecd8c7
medlb <72205419+medlb@users.noreply.github.com>
2021-02-05 15:27:03 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-function nextPage(page){if(page==="page1"){document.getElementById("mainBtn").remove();var mainQuest=document.getElementById("mainQuest");mainQuest.innerHTML="<h1>تحضير بعض الأسئلة<span id='wait'></span></h1>";var dotsInterv=window.setInterval(function(){var wait=document.getElementById("wait");if(wait.innerHTML.length>=3)
+function nextPage(page){if(page==="page1"){document.getElementById("mainBtn").remove();var mainQuest=document.getElementById("mainQuest");mainQuest.innerHTML="<h1>Preparing some questions <span id='wait'></span></h1>";var dotsInterv=window.setInterval(function(){var wait=document.getElementById("wait");if(wait.innerHTML.length>=3)
 wait.innerHTML="";else
 wait.innerHTML+=".";},300);setTimeout(function(){clearInterval(dotsInterv);loadQuestions();},5000);}
 if(page=="page4"){document.getElementById("steps_progress").src="img/progress_4.png";document.getElementById("channel-step").remove();var element=document.getElementById("question");element.innerHTML="";document.getElementById("account-step").style.display="block";}
@@ -7,17 +7,16 @@ document.getElementById("account-step").remove();document.getElementById("loader
 if(page=="page6"){window.location='#';}}
 function loadQuestions(){document.getElementById("mainQuest").innerHTML="";document.getElementsByClassName("main-question")[0].remove();document.getElementById("progbars").style.textAlign="center";document.getElementById("progbars").style.display='block';populate();}
 function Question(text,choices){this.text=text;this.choices=choices;}
-var questions=[new Question("Are you ready to work 6 hours a day ?",["Yes","No"]),new Question("What is your level of education ?",["University","I can write and read"]),new Question("اختر احدى المجالات التالية التي تناسب هوايتك او تخصصك  ؟",[" Marketing "," Writing Articles "," CCTV Monitor "," Customer Service "])];function Quiz(questions){this.questions=questions;this.questionIndex=0;}
+var questions=[new Question("Are you ready to work 6 hours a day ?",["Yes","No"]),new Question("What is your level of education ?",["University","I can write and read"]),new Question("Choose one of the following areas that suit your hobby or specialty ",[" Marketing "," Writing Articles "," CCTV Monitor "," Customer Service "])];function Quiz(questions){this.questions=questions;this.questionIndex=0;}
 var quiz=new Quiz(questions);Quiz.prototype.getQuestionIndex=function(){return this.questions[this.questionIndex];}
 Quiz.prototype.isEnded=function(){return this.questions.length<=this.questionIndex;}
 Quiz.prototype.guess=function(){this.questionIndex++;}
 function populate(){var element=document.getElementById("question");if(!quiz.isEnded()){element.innerHTML=quiz.getQuestionIndex().text;var choices=quiz.getQuestionIndex().choices;for(var i=0;i<choices.length;i++){var element=document.getElementById("choice"+i);element.innerHTML=choices[i];guess("btn"+i);}
 showProgress();}
-else{document.getElementById("steps_progress").src="img/progress_4.png";nextPage("page4");hideProgress();for(var i=0;i<4;i++){document.getElementById("btn"+i).style.display="none";}}
+else{nextPage("page4");hideProgress();for(var i=0;i<4;i++){document.getElementById("btn"+i).style.display="none";}}
 console.log("Is Ended?:"+quiz.isEnded());}
 function guess(id){var button=document.getElementById(id);button.onclick=function(){quiz.guess();console.log();populate();}}
-function showProgress(){var currQuestNum=quiz.questionIndex+1;if(currQuestNum==2){document.getElementById("steps_progress").src="img/progress_2.png";}
-else if(currQuestNum==3){document.getElementById("steps_progress").src="img/progress_3.png";}}
+function showProgress(){var currQuestNum=quiz.questionIndex+1;}
 function hideProgress(){document.getElementById("quest_label").innerHTML="";}
-function showLastSteps(){var element=document.getElementById("question");element.innerHTML="يجب عليك متابعة احد القنوات"
+function showLastSteps(){var element=document.getElementById("question");element.innerHTML="#"
 document.getElementById("channel-step").style.display="block";}
